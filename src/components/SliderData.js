ffc74b07fa5173cf58384a900d9a0e7e9f1d80db
Nick Hale <datfuriouskid@live.com>
2021-08-30 17:08:53 -0700

    entire app

---
+++ src/components/SliderData.js
@@ -0,0 +1,137 @@
+export const SliderData = [
+  {
+    image: "../images/knull-image-1.jpeg",
+  },
+  {
+    image: "../images/knull-image-2.jpeg",
+  },
+  {
+    image: "../images/knull-image-3.jpeg",
+  },
+  {
+    image: "../images/knull-image-4.jpeg",
+  },
+  {
+    image: "../images/knull-image-5.jpeg",
+  },
+  {
+    image: "../images/knull-image-6.jpeg",
+  },
+  {
+    image: "../images/knull-image-7.jpeg",
+  },
+  {
+    image: "../images/knull-image-8.jpeg",
+  },
+  {
+    image: "../images/knull-image-9.jpeg",
+  },
+  {
+    image: "../images/knull-image-10.jpeg",
+  },
+  {
+    image: "../images/knull-image-11.jpeg",
+  },
+  {
+    image: "../images/knull-image-12.jpeg",
+  },
+  {
+    image: "../images/knull-image-13.jpeg",
+  },
+  {
+    image: "../images/knull-image-14.jpeg",
+  },
+  {
+    image: "../images/knull-image-15.jpeg",
+  },
+  {
+    image: "../images/knull-image-16.jpeg",
+  },
+  {
+    image: "../images/knull-image-17.jpeg",
+  },
+  {
+    image: "../images/knull-image-18.jpeg",
+  },
+  {
+    image: "../images/knull-image-19.jpeg",
+  },
+  {
+    image: "../images/knull-image-20.jpeg",
+  },
+  {
+    image: "../images/knull-image-21.jpeg",
+  },
+  {
+    image: "../images/knull-image-22.jpeg",
+  },
+  {
+    image: "../images/knull-image-23.jpeg",
+  },
+  {
+    image: "../images/knull-image-24.jpeg",
+  },
+  {
+    image: "../images/knull-image-25.jpeg",
+  },
+  {
+    image: "../images/knull-image-26.jpeg",
+  },
+  {
+    image: "../images/knull-image-27.jpeg",
+  },
+  {
+    image: "../images/knull-image-28.jpeg",
+  },
+  {
+    image: "../images/knull-image-29.jpeg",
+  },
+  {
+    image: "../images/knull-image-30.jpeg",
+  },
+  {
+    image: "../images/knull-image-31.jpeg",
+  },
+  {
+    image: "../images/knull-image-32.jpeg",
+  },
+  {
+    image: "../images/knull-image-33.jpeg",
+  },
+  {
+    image: "../images/knull-image-34.jpeg",
+  },
+  {
+    image: "../images/knull-image-35.jpeg",
+  },
+  {
+    image: "../images/knull-image-36.jpeg",
+  },
+  {
+    image: "../images/knull-image-37.jpeg",
+  },
+  {
+    image: "../images/knull-image-38.jpeg",
+  },
+  {
+    image: "../images/knull-image-39.jpeg",
+  },
+  {
+    image: "../images/knull-image-40.jpeg",
+  },
+  {
+    image: "../images/knull-image-41.jpeg",
+  },
+  {
+    image: "../images/knull-image-42.jpeg",
+  },
+  {
+    image: "../images/knull-image-43.jpeg",
+  },
+  {
+    image: "../images/knull-image-44.jpeg",
+  },
+  {
+    image: "../images/knull-image-1.jpeg",
+  },
+];
